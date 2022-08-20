069b8d1db52d63b7346762a1fcad5759ca8e6bc8
Geo Ste <stein.geoffrey@gmail.com>
2022-08-20 02:04:11 +0200

    Added Github URI

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Chew WP NoBullshit
-~Current Version:1.0.1~
 
 ## Updater
-This plugin uses the following repo for WordPress updates : https://github.com/radishconcepts/WordPress-GitHub-Plugin-Updater
--- chew-wp-nobullshit.php
+++ chew-wp-nobullshit.php
@@ -3,6 +3,7 @@
 /*
 Plugin Name: Chew WP No Bullshit
 Plugin URI: https://www.startingup.fr/chew-wp-no-bullshit
+Github Plugin URI: https://github.com/startingup-tech/chew-wp-nobullshit
 Description: Optimizes WordPress installations by removing useless stuff.
 Version: 1.0.1
 Author: Geoffrey Stein
@@ -10,25 +11,6 @@ Author URI: https://www.geoffrey-stein.fr
 Text Domain: clean bullshit fresh
 Domain Path: /lang
 */
-
-require_once(__DIR__ . '/updater.php');
-
-if (is_admin()) {
-    $config = [
-        'slug' => plugin_basename(__FILE__),
-        'proper_folder_name' => 'chew-wp-nobullshit',
-        'api_url' => 'https://api.github.com/repos/startingup-tech/chew-wp-nobullshit',
-        'raw_url' => 'https://raw.github.com/startingup-tech/chew-wp-nobullshit',
-        'github_url' => 'https://github.com/startingup-tech/chew-wp-nobullshit',
-        'zip_url' => 'https://github.com/startingup-tech/chew-wp-nobullshit/zipball/master',
-        'sslverify' => true,
-        'requires' => '5.9',
-        'tested' => '5.9',
-        'readme' => 'README.md',
-        'access_token' => '',
-    ];
-    new WP_GitHub_Updater($config);
-}
 
 require_once(__DIR__ . '/src/ChewRecipeInterface.php');
 require_once(__DIR__ . '/src/AbstractChewRecipe.php');
